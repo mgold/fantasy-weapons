b86f83e7ffbc1a0bfd40cefc54d8b0d04aca7300
Max Goldstein <maxgoldstein1@gmail.com>
2016-03-18 17:05:03 -0400

    Add embellishments

--- Weapon.elm
+++ Weapon.elm
@@ -12,8 +12,18 @@ type Weapon
   = Wand WandInfo
 
 
+type Embellishment
+  = Plain
+  | EndGrips
+  | Pommel Color
+  | HiltGems Color
+  | BladeLine Color
+  | HiltLine Color
+  | Crossguard Color
+
+
 type alias WandInfo =
-  { primary : Color, accent : Color, length : Float, width : Float }
+  { primary : Color, accent : Color, length : Float, width : Float, embellishment : Embellishment }
 
 
 genBrown : Generator Color
@@ -26,8 +36,62 @@ genBrown =
 
 init : ParticleSystem -> Generator Weapon
 init system =
-  Random.map4 WandInfo genBrown genBrown (Random.float 150 280) (Random.float 2 12)
+  Random.map5 WandInfo genBrown genBrown (Random.float 150 280) (Random.float 2 12) (genEmbellishment system)
+    |> Random.map verifyEmbellishment
     |> Random.map Wand
+
+
+genEmbellishment : ParticleSystem -> Generator Embellishment
+genEmbellishment { primary, accent } =
+  let
+    choose clr i =
+      case i of
+        1 ->
+          EndGrips
+
+        2 ->
+          Pommel clr
+
+        3 ->
+          HiltGems clr
+
+        4 ->
+          BladeLine clr
+
+        5 ->
+          HiltLine clr
+
+        6 ->
+          Crossguard clr
+
+        7 ->
+          EndGrips
+
+        _ ->
+          Plain
+  in
+    Random.map2
+      choose
+      (Random.choice primary accent)
+      (Random.int 0 9)
+
+
+{-| Not all embellishments look good on thin wands.
+-}
+verifyEmbellishment : WandInfo -> WandInfo
+verifyEmbellishment wand =
+  if wand.width > 5 then
+    wand
+  else
+    case wand.embellishment of
+      Crossguard _ ->
+        { wand | embellishment = Plain }
+
+      BladeLine _ ->
+        { wand | embellishment = EndGrips }
+
+      _ ->
+        wand
 
 
 length : Weapon -> Float
@@ -62,7 +126,7 @@ view t weapon =
 
 
 drawWand : Time -> WandInfo -> Collage.Form
-drawWand t { primary, accent, length, width } =
+drawWand t ({ primary, accent, length, width } as wand) =
   let
     handleLength =
       0.3 * length
@@ -70,6 +134,7 @@ drawWand t { primary, accent, length, width } =
     [ rect length width |> filled primary
     , rect handleLength (1.5 * width) |> filled accent |> Collage.moveX (-0.35 * length)
     , circle (width / 2.3) |> filled primary |> Collage.moveX (length / 2)
+    , embellishmentForm wand
     ]
       |> rotateAroundX
           (length / 2.4)
@@ -124,3 +189,49 @@ rotateAroundX centerOfRotation angle forms =
         `Transform2D.multiply` moveOut
   in
     Collage.groupTransform tform forms
+
+
+embellishmentForm : WandInfo -> Collage.Form
+embellishmentForm wand =
+  case wand.embellishment of
+    EndGrips ->
+      let
+        grip =
+          rect 5 (wand.width * 2.4) |> filled wand.accent
+      in
+        Collage.group
+          [ grip |> Collage.moveX (-wand.length / 2)
+          , grip |> Collage.moveX (-wand.length * 0.2)
+          ]
+
+    Pommel clr ->
+      Collage.group
+        [ circle (1.4 * wand.width) |> filled wand.accent
+        , circle (0.7 * wand.width) |> filled clr
+        ]
+        |> Collage.moveX (-wand.length / 2)
+
+    HiltGems clr ->
+      let
+        gem =
+          circle (0.3 * wand.width) |> filled clr
+      in
+        List.map (\dist -> Collage.moveX (-wand.length * dist) gem) [ 0.45, 0.35, 0.25 ]
+          |> Collage.group
+
+    BladeLine clr ->
+      rect (0.7 * wand.length) 1 |> filled clr |> move ( wand.length * 0.15, -2 )
+
+    HiltLine clr ->
+      rect (0.3 * wand.length) 1 |> filled clr |> move ( -wand.length * 0.35, 2 )
+
+    Crossguard clr ->
+      [ rect 6 (8 * wand.width |> min 50) |> filled Color.charcoal
+      , ngon 3 10 |> filled Color.charcoal |> Collage.rotate pi |> Collage.moveX -3
+      , circle 2.5 |> filled clr |> Collage.moveX -2
+      ]
+        |> Collage.group
+        |> Collage.moveX (-wand.length * 0.2)
+
+    _ ->
+      Collage.group []
